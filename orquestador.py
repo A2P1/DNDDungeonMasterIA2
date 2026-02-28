46df38fa28d4e1bd5a2d8ca7670e567a5ee7ca6e
Pablo Azpitarte Gutiérrez <pabloazpitartegutierrez@gmail.com>
2026-02-28 12:05:05 +0100

    Agente combate llamado como tool

--- orquestador.py
+++ orquestador.py
@@ -14,10 +14,12 @@ def get_status():
     with open('stats.json', 'r', encoding="utf-8") as f:
         return f.read()
 # Funciona como un orquestador lógico programado por mí para que siempre llame al narrador
+
 @tool
 def llamar_combate():
-    combate = 1
-    return combate
+    """Úsala cuando el usuario quiera atacar, pelear o iniciar un enfrentamiento."""
+    return "SEÑAL_INICIAR_COMBATE"
+
 
     
 
@@ -30,8 +32,8 @@ def main():
         
     ]
     EnCombate = 0
-    llm_tools = llm.bind_tools([get_status])
-    Herramientas = [get_status]
+    llm_tools = llm.bind_tools([get_status, llamar_combate])
+    Herramientas = [get_status, llamar_combate]
     mapa_herramientas = {t.name: t for t in Herramientas}
 
 
@@ -48,7 +50,30 @@ def main():
                         # Buscamos la función en nuestro mapa y la ejecutamos
                         seleccionada = mapa_herramientas[tool_call["name"]]
                         respuesta_herramienta = seleccionada.invoke(tool_call["args"])
-                        
+                        # Si de todas las herramientas llama al combate
+                        if tool_call["name"] == "llamar_combate":
+                            #resultado_validacion = llamar_combate.invoke({"resumen_actual": resumen})
+                            #if "COMBATE PERMITIDO" in resultado_validacion:
+                            EnCombate = 1 # Asignamos el flag EnCombate a 1
+                            print("\n--- ¡ESPADAS FUERA! EL COMBATE COMIENZA ---\n")
+                            while EnCombate == 1:
+                                accion = input("Qué acción quieres realizar? (atacar o huir)")
+                                resultado, valor = combate(accion, datos)
+                                #messages.append(HumanMessage(content=combate(accion))) # Guardamos la información del combate para que el narrador pueda procesarla y generar un resumen coherente
+                                finalizado = llm_tools.invoke([
+                                    SystemMessage(content=f"Si el combate ha terminado, guarda la palabra 'FINALIZADO' en la variable finalizado"),
+                                    HumanMessage(content=resultado)
+                                ]).content
+                                if "FINALIZADO" in finalizado.upper():
+                                    comentario = llm_tools.invoke([
+                                        SystemMessage(content=f"el jugador ha sacado un {valor} al tirar los dados en un D&D. Si es mayor a 12 ha acertado, si no, ha fracasado. Narra esto de forma genérica en 1 frase mostrando el valor del dado teniendo en cuenta este resultado: {resultado} y guardalo en la variable 'comentario'")
+                                    ])
+                                print(comentario.content)
+                                EnCombate = 0
+                        '''else:
+                            respuesta_herramienta = resultado_validacion'''
+                        # DE AQUÍ
+
                         # 3. Le pasamos el resultado a la IA para que lo procese
                         from langchain_core.messages import ToolMessage
                         
@@ -65,10 +90,12 @@ def main():
                         ])
                         
                         print(respuesta_final.content)
+                        # HASTA AQUÍ ES FIJO PARA TODAS LAS TOOLS
+
             else:
                 if user_input.lower() == "salir":
                     break
-                else:
+                    ''' else:
                     messages.append(HumanMessage(content=user_input)) # Guardamos la info que ha introducido el usuario para procesarla
                     decision = llm_tools.invoke([
                         SystemMessage(content=f"Si el usuario menciona alguna de estas palabras: combate, lucha, pelea, ataco, ataque o enfrento, guarda la palabra 'COMBATE' en la variable decision" ),
@@ -93,8 +120,9 @@ def main():
                                 print(comentario.content)
                                 EnCombate = 0
 
-                    else:
-                        print(narrador(resumen, user_input)) # Hacemos la llamada al narrador pasándole el resumen y la información introducida por el usuario
+                '''
+                else:
+                    print(narrador(resumen, user_input)) # Hacemos la llamada al narrador pasándole el resumen y la información introducida por el usuario
         else:
             print(narrador_inicio())
 main()
